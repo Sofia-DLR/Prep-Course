91eb759655b8d12c8f6fb4b5f06bf82a672c9982
Sofia-DLR <delarosa.arely@outlook.com>
2022-08-21 19:44:36 -0500

    First commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -121,7 +121,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código: 
-  return Math.pow(num, 2); 
+  return num * num; 
   
 }
 
